faa7351a0211f9c7e56c8c6ee4832237bc4ac286
Atari <jager@djem.ru>
2021-12-01 09:57:43 +0300

    Day 01

--- day01/a.js
+++ day01/a.js
@@ -2,5 +2,6 @@ const fs = require('fs');
 let data = fs.readFileSync('input.txt', { encoding: 'utf8' });
 
 const counter = data
-    .split(/\r?\n/g)
-    .reduce((acc, v, i, arr) => i ? acc + (Number(v) > Number(arr[i - 1])) : acc, 0);
+  .split(/\r?\n/g)
+  .map(Number)
+  .reduce((acc, v, i, arr) => i ? acc + (v > arr[i - 1]) : acc, 0);
--- day01/b.js
+++ day01/b.js
@@ -3,4 +3,5 @@ let data = fs.readFileSync('input.txt', { encoding: 'utf8' });
 
 const counter = data
   .split(/\r?\n/g)
-  .reduce((acc, v, i, arr) => i > 2 ? acc + (Number(v) > Number(arr[i - 3])) : acc, 0);
+  .map(Number)
+  .reduce((acc, v, i, arr) => i > 2 ? acc + (v > arr[i - 3]) : acc, 0);
