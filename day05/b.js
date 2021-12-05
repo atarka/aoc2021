eabcc00a08e7e46d784a4536929927c4a886f789
Atari <jager@djem.ru>
2021-12-05 18:26:14 +0300

    Day 05 strip the marble

--- day05/b.js
+++ day05/b.js
@@ -3,13 +3,13 @@ let data = fs.readFileSync('input.txt', { encoding: 'utf8' });
 
 const points = {};
 data.split(/\r?\n/g).forEach(line => {
-  const [x1, y1, x2, y2] = line.split(/\D+/).map(Number);
-  const shiftX = x2 === x1 ? 0 : (x2 - x1) / Math.abs(x2 - x1);
-  const shiftY = y2 === y1 ? 0 : (y2 - y1) / Math.abs(y2 - y1);
-  for (let x = x1, y = y1;; x += shiftX, y += shiftY) {
+  let [x, y, x2, y2] = line.split(/\D+/).map(Number);
+  const shiftX = Math.sign(x2 - x);
+  const shiftY = Math.sign(y2 - y);
+  for (let toX = x2 + shiftX, toY = y2 + shiftY; x !== toX || y !== toY; x += shiftX, y += shiftY)
     points[`${x}:${y}`] = (points[`${x}:${y}`] || 0) + 1;
-    if (x === x2 && y === y2) break;
-  }
 });
 
 const overlap = Object.keys(points).reduce((acc, key) => acc + (points[key] >= 2), 0);
+
+console.log(overlap);
